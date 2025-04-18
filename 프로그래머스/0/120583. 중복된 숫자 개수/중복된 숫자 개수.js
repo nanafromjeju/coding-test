403bc3fa3e42c2c7de051677234df89cd4876e63
NANA <nanafromjeju@gmail.com>
2025-04-18 19:24:03 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -0,0 +1,10 @@
+function solution(array, n) {
+    let count = 0
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === n) {
+            count++
+        }
+    }
+    return count;
+}
