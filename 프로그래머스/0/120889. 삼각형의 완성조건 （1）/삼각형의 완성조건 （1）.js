479bf92f9c54f9458d0c66941fa24bfe7e4dae5c
NANA <nanafromjeju@gmail.com>
2025-05-04 23:56:07 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -0,0 +1,11 @@
+function solution(sides) {
+    sides.sort((a, b) => a - b)
+    
+    if (sides[2] < sides[0] + sides[1]) {
+        return 1
+    } else {
+        return 2
+    }
+    
+    return answer;
+}
