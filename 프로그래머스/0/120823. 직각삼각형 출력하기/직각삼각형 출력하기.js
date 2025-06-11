a9bcfbd88e10bcdcc3a12f0e41fe2f33fd684470
NANA <nanafromjeju@gmail.com>
2025-06-11 22:41:15 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 166.45 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', function (line) {
+    const n = Number(line);
+    
+    for (let i = 1; i <= n; i++) {
+        console.log("*".repeat(i));
+    }
+    
+    rl.close();
+});
