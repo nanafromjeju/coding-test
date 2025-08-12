75af4a936ccfadd7f73222dc7180fe04d82e1fb5
NANA <nanafromjeju@gmail.com>
2025-08-13 00:40:45 +0900

    [level 0] Title: 숫자 찾기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
@@ -0,0 +1,11 @@
+function solution(num, k) {
+    const numStr = String(num);
+    
+    for (let i = 0; i < numStr.length; i++) {
+        if (parseInt(numStr[i]) === k) {
+            return i + 1;
+        }
+    }
+    
+    return -1;
+}
