8b6929fdd95c81e034e8d5bc71ef017b70139a3a
NANA <nanafromjeju@gmail.com>
2026-02-05 23:23:28 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,12 +1,11 @@
 function solution(num_list) {
-    const last = num_list[num_list.length - 1];  // 마지막 원소
-    const prev = num_list[num_list.length - 2];  // 그전 원소
+    const last = num_list[num_list.length - 1];
+    const prev = num_list[num_list.length - 2];
     
     if (last > prev) {
-        num_list.push(last - prev);  // 차이값 추가
+        num_list.push(last - prev);
     } else {
-        num_list.push(last * 2);     // 2배 값 추가
+        num_list.push(last * 2);
     }
-    
     return num_list;
 }
